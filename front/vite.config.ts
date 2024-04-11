c782394594a6de9529d85c5752cb1c909c976aa0
hyeonKii <khk5676@gmail.com>
2024-04-11 22:44:32 +0900

    feat: scss 초기설정

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {find: "components", replacement: "/src/components"},
+      {find: "pages", replacement: "/src/pages"},
+      {find: "firebaseApp", replacement: "/src/firebaseApp"},
+      {find: "context", replacement: "/src/context"}
+    ]
+  }
 })
