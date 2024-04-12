1341d361a96439befbf614ee20fdbb6fdd599b10
hyeonKii <khk5676@gmail.com>
2024-04-12 15:42:55 +0900

    feat: Home scss 작업 완료

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -1,3 +1,13 @@
-export function Layout() {
-  return <></>;
+import {ReactNode} from "react";
+
+interface LayoutProps {
+    children: ReactNode;
 }
+
+export const Layout = ({children}: LayoutProps) => {
+    return (
+        <>
+            <div className="layout">{children}</div>
+        </>
+    );
+};
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Router>
