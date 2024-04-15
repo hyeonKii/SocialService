55d2f7f386d369c66284fabb9d2002bb081284ba
hyeonKii <khk5676@gmail.com>
2024-04-15 17:09:43 +0900

    feat: 게시글 생성 및 수정 삭제 버튼 유무 설정

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -1,5 +1,9 @@
 import PostBox from "components/posts/PostBox";
 import PostForm from "components/posts/PostForm";
+import AuthContext from "context/AuthContext";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useEffect, useState } from "react";
 
 export interface PostProps {
   id: string;
@@ -13,64 +17,44 @@ export interface PostProps {
   comments?: any;
 }
 
-const posts: PostProps[] = [
-  {
-    id: "1",
-    email: "test@test.com",
-    content: "dummyData",
-    createdAt: "2024-4-12",
-    uid: "01",
-  },
-  {
-    id: "2",
-    email: "test@test.com",
-    content: "dummyData",
-    createdAt: "2024-4-12",
-    uid: "01",
-  },
-  {
-    id: "3",
-    email: "test@test.com",
-    content: "dummyData",
-    createdAt: "2024-4-12",
-    uid: "01",
-  },
-  {
-    id: "4",
-    email: "test@test.com",
-    content: "dummyData",
-    createdAt: "2024-4-12",
-    uid: "01",
-  },
-  {
-    id: "5",
-    email: "test@test.com",
-    content: "dummyData",
-    createdAt: "2024-4-12",
-    uid: "01",
-  },
-  {
-    id: "6",
-    email: "test@test.com",
-    content: "dummyData",
-    createdAt: "2024-4-12",
-    uid: "01",
-  },
-];
-
 export default function HomePage() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      let postRef = collection(db, "posts");
+      let postsQuery = query(postRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user]);
+
   return (
     <div className="home">
-      <div className="home__title">Home</div>
-      <div className="home__tabs">
-        <div className="home__tab home__tab--active">For You</div>
-        <div className="home__tab">Following</div>
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Following</div>
+        </div>
       </div>
+
       <PostForm />
       <div className="post">
-        {posts?.map((post) => (
-          <PostBox post={post} key={post.id}/>
-        ))}
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
       </div>
     </div>
   );
