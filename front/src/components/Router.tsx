1ea505422dc9fa34a9d79a4277e2ea09b7781508
hyeonKii <khk5676@gmail.com>
2024-06-25 15:17:00 +0900

    chore: unused file delete

--- front/src/components/Router.tsx
+++ front/src/components/Router.tsx
@@ -1,9 +1,7 @@
 import HomePage from "pages/home";
 import NotificationsPage from "pages/notifications";
-import PostListPage from "pages/posts";
 import PostDetail from "pages/posts/detail";
 import PostEdit from "pages/posts/edit";
-import PostNew from "pages/posts/new";
 import ProfilePage from "pages/profile";
 import ProfileEdit from "pages/profile/edit";
 import SearchPage from "pages/search";
@@ -21,9 +19,7 @@ export default function Router({ isAuth }: RouterProps) {
       {isAuth ? (
         <>
           <Route path="/" element={<HomePage />} />
-          <Route path="/posts" element={<PostListPage />} />
           <Route path="/posts/:id" element={<PostDetail />} />
-          <Route path="/posts/new" element={<PostNew />} />
           <Route path="/posts/edit/:id" element={<PostEdit />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/profile/edit" element={<ProfileEdit />} />
